2008adc8deacb50909b0ed759246524c07738e08
Nikita Martynov <nmartynov@rdp.ru>
2018-07-09 16:27:53 +0300

    dont generate url_path for blocktype_ip

--- rknscan.py
+++ rknscan.py
@@ -476,9 +476,15 @@ else:
 
         for ip in ips_c:
             ips.append(ip.text)
-        for sub in subs_c:
-            for ip in IPNetwork(sub.text):
-                ips.append(str(ip))
+        try:
+            blocktype = content.attrib['blockType']
+        except KeyError:
+            blocktype = "default"
+ 
+        if blocktype != 'ip':
+            for sub in subs_c:
+                for ip in IPNetwork(sub.text):
+                    ips.append(str(ip))
 
         domains = content.findall('domain')
         urls = content.findall('url')
